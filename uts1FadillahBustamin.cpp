322a8bd68ed8a75b3ce0e220d074621e770f3edc
Nur Fadillah Bustamin <48977915+FadillahBustamin@users.noreply.github.com>
2019-03-31 19:53:01 +0700

    Add files via upload

---
+++ uts1FadillahBustamin.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+    cout <<"Nama : Nur Fadillah Butamin"<<endl;
+    cout <<"NIM  : 1829140008"<<endl;
+    cout <<"Kelas: TEKOM B"<<endl;
+    cout <<"Tahun: 2018"<<endl;
+
+    return 0;
+ }
